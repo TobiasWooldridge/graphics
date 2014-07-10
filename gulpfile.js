9a38c65b66afe875ca709701d59d89a49cdef1ff
Tobias Wooldridge <tobias@wooldridge.id.au>
2014-07-11 03:40:09 +0930

    gulp++

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    notify = require('gulp-notify'),
+    imagemin = require('gulp-imagemin'),
+    less = require('gulp-less');
+
+gulp.task('styles', function() {
+    return gulp.src('src/css/*.*ss')
+        .pipe(less())
+        .pipe(rename({extname: ".css"}))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('vendors', function() {
+    gulp.src(['vendor/*.js'])
+        .pipe(gulp.dest('dist/vendor/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/vendor/js'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('src/js/**/*.js*')
+//        .pipe(jshint('.jshintrc'))
+//        .pipe(jshint.reporter('default'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('textures', function() {
+    return gulp.src('src/textures/**/*')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/textures'));
+});
+
+gulp.task('html', function() {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('objects', function() {
+    return gulp.src('src/objects/*')
+        .pipe(gulp.dest('dist/objects/'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src(['dist/css', 'dist/js', 'dist'], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('default', ['clean'], function() {
+    return gulp.start('html', 'styles', 'scripts', 'vendors', 'textures', 'objects');
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/css/**/*', ['styles']);
+    gulp.watch('src/js/**/*', ['scripts']);
+    gulp.watch('src/textures/**/*', ['textures']);
+    gulp.watch('src/**/*.html', ['html']);
+
+    gulp.start('default');
+});
+
